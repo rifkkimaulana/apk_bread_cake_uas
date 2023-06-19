139a9f1d105bbd0958a1c1784be4e82b65cce590
rifkkimaulana <rifkkimaulana@gmail.com>
2023-06-19 21:43:35 +0700

    update

---
+++ admin/dashboard/content-header.php
@@ -0,0 +1,11 @@
+<div class="row mb-2">
+    <div class="col-sm-6">
+        <h1 class="m-0">Dashboard</h1>
+    </div>
+    <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="dashboard.php?page=dashboard">Home</a></li>
+            <li class="breadcrumb-item active">Dashboard</li>
+        </ol>
+    </div>
+</div>
