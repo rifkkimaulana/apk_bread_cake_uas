af5e35bb0de23089c5125526cef8a09562a1f243
rifkkimaulana <rifkkimaulana@gmail.com>
2023-06-19 17:15:28 +0700

    update database

--- database/db_bnc_uas.sql
+++ database/db_bnc_uas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 19 Jun 2023 pada 00.42
+-- Waktu pembuatan: 19 Jun 2023 pada 12.14
 -- Versi server: 10.4.25-MariaDB
 -- Versi PHP: 7.4.30
 
@@ -24,6 +24,47 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `tb_kategori_produk`
+--
+
+CREATE TABLE `tb_kategori_produk` (
+  `id` int(11) NOT NULL,
+  `kategori_produk` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_kategori_produk`
+--
+
+INSERT INTO `tb_kategori_produk` (`id`, `kategori_produk`) VALUES
+(2, 'Cake');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_produk`
+--
+
+CREATE TABLE `tb_produk` (
+  `id` int(11) NOT NULL,
+  `nama_produk` varchar(100) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `deskripsi` varchar(500) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `image` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_produk`
+--
+
+INSERT INTO `tb_produk` (`id`, `nama_produk`, `id_kategori`, `deskripsi`, `harga`, `stok`, `image`) VALUES
+(3, 'Magnam Tiste', 2, 'deskripsi: Magnam Tiste', 20000, 20, 'upload_20230619113830_menu-item-1.png');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `tb_users`
 --
 
@@ -40,12 +81,23 @@ CREATE TABLE `tb_users` (
 --
 
 INSERT INTO `tb_users` (`id`, `nama_operator`, `username`, `password`, `email`) VALUES
-(1, 'Kelompok 6', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'rifkkimaulana@gmail.com'),
-(3, 'nma', 'asdf', '7815696ecbf1c96e6894b779456d330e', 'em');
+(1, 'Kelompok 6', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'rifkkimaulana@gmail.com');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `tb_kategori_produk`
+--
+ALTER TABLE `tb_kategori_produk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_produk`
+--
+ALTER TABLE `tb_produk`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `tb_users`
@@ -56,6 +108,18 @@ ALTER TABLE `tb_users`
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_kategori_produk`
+--
+ALTER TABLE `tb_kategori_produk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_produk`
+--
+ALTER TABLE `tb_produk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_users`
