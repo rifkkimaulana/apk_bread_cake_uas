e5b523ca1f6af3664d2d681103df090aaed81346
rifkkimaulana <rifkkimaulana@gmail.com>
2023-06-30 20:49:50 +0700

    new

--- forms/contact.php
+++ forms/contact.php
@@ -1,22 +1,18 @@
 <?php
 include_once("../config/config.php");
 
-// Ambil data dari formulir
 $nama_lengkap = $_POST['name'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
 $pesan = $_POST['message'];
-$status = 'belum dibaca'; // Set default status
+$status = 'belum dibaca';
 
-// Query untuk menyimpan data ke tabel tb_pesan
 $query = "INSERT INTO tb_pesan (nama_lengkap, email, subject, pesan, status) VALUES ('$nama_lengkap', '$email', '$subject', '$pesan', '$status')";
 $result = mysqli_query($mysqli, $query);
 
 if ($result) {
-  // Data berhasil disimpan, arahkan kembali ke halaman utama
   echo '<script>alert("Pesan berhasil dikirim!"); window.location.href = "../";</script>';
   exit();
 } else {
-  // Terjadi kesalahan. Tampilkan pesan error atau lakukan tindakan yang sesuai.
 }
 ?>
